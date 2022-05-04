18c64745bb8c9195a4c00e2ef5ff168735242fce
Thiago Ausechi <thiago_ausechi@icloud.com>
2022-05-04 14:02:42 -0300

    v1.0.4

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -22,7 +22,7 @@ function App()
   {
     const getPersonas = async () => 
     {
-      const data = getPersonalities();
+      const data = await getPersonalities();
       if (data.docs.length > 0)
       {
         const randomized =
