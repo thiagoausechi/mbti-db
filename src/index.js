63f038e1d30ff931ca02adf6f6b33f1b19fe66e1
Thiago Ausechi <thiago_ausechi@icloud.com>
2022-09-09 14:36:14 -0300

    Emptying the branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./global/styles.css";
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./pages/App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
--- src/lib/dev_env.js
+++
@@ -1,1 +0,0 @@
-export const isDevEnv = true && (!process.env.NODE_ENV || process.env.NODE_ENV === 'development');
