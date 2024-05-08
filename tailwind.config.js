a96e9c698af365405a4493c6dbfdb563174f7ef1
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-05-08 16:12:19 -0300

    fix: 🐛 add tailwind content configuration so it reads components

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [],
+  content: ["src/**/*.{ts,tsx}"],
   theme: {
     container: {
       center: true,
