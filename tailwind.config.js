c65c42efa3810ae86fb62b502bb0079fbba5687d
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-05-08 14:28:01 -0300

    chore(config): 🔧 add style to the tailwind container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
   },
   plugins: [],
 };
