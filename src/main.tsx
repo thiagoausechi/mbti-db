483fdbbb767093ce42d53e98617e803c34be2add
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-05-08 16:09:00 -0300

    feat: ✨ add the globals styles to the main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { Router } from "@/routes";
+import "@/styles/globals.css";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<Router />);
