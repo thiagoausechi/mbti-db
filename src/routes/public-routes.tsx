632a6e27cc82f5218e521f16d4832a2a71164966
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-05-08 18:05:42 -0300

    fix: 🚨 fix public routes to properly imports the pages components

--- src/routes/public-routes.tsx
+++ src/routes/public-routes.tsx
@@ -1,5 +1,5 @@
+import { Home } from "@/pages";
 import { Navigate, RouteObject } from "react-router-dom";
-import { Home } from "../pages/home";
 
 export const publicRoutes = (): Array<RouteObject> => [
   { path: "/home", element: <Home /> },
