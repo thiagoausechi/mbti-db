69216035b789a2d29b8f8e2df3e347f458a00be9
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-05-08 14:11:49 -0300

    fix: 🚨 fix main component to properly imports the router component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
+import { Router } from "@/routes";
 import ReactDOM from "react-dom/client";
-import { Router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<Router />);
