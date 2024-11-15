db411dc7430bb9b315840aa8be11ec8e53345d4c
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-11-15 14:41:50 -0300

    chore: 🔧 update the `container` default settings in Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-opensans)", ...fontFamily.sans],
