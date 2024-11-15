492d0ee6dd00d6a8e32c8dc5b2825a5ae7cb4193
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-11-15 13:56:35 -0300

    feat: ✨ add base layout for the site

---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import "~/client/styles/globals.css";
+
+import { type Metadata } from "next";
+import type { PropsWithChildren } from "react";
+
+export const metadata: Metadata = {
+  title: "mbti db",
+  applicationName: "mbti db",
+  creator: "Thiago Ausechi",
+  description: "Um acervo de personalidades com base no MBTI",
+  keywords: [
+    "mbti",
+    "Myers-Briggs Type Indicator",
+    "database",
+    "banco de dados",
+    "collection",
+    "acervo",
+    "personalidades",
+    "personalities",
+    "rede social",
+    "social network",
+    "social",
+  ],
+  manifest: "/site.webmanifest",
+  appleWebApp: {
+    title: "MBTI db",
+  },
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "/favicon/favicon-96x96.png",
+      sizes: "96x96",
+    },
+    { rel: "icon", type: "image/svg+xml", url: "/favicon/favicon.svg" },
+    { rel: "shortcut", url: "/favicon/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      url: "/favicon/apple-touch-icon.png",
+      sizes: "180x180",
+    },
+  ],
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
