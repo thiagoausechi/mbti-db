479ca937d8a9a0d86e955ab23e43ca51cec92d71
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-11-15 17:29:22 -0300

    feat: ✨ add utility function `cn` for merging class names

---
+++ src/client/utils.ts
@@ -0,0 +1,7 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call */
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
