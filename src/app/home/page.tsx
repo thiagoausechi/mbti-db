dde351e74419638b2715dfe9886c8650f2056aaa
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-11-18 22:25:16 -0300

    refactor: ♻️ create home page component and update index page to redirect to home

---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="container">
+      <p>Hello, home page!</p>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function HomePage() {
-  return <main>Hello, world!</main>;
+import { home } from "~/router/routes";
+import { redirectTo } from "~/router/utils";
+
+export default function IndexPage() {
+  return redirectTo(home);
 }
