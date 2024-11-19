69ba87ea7df0be07d72b0724aa003dee924bc6bf
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-11-18 21:58:45 -0300

    feat: ✨ add `twx` utility using `createTwc` for enhanced Tailwind class management

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -1,7 +1,10 @@
 import { type ClassValue, clsx } from "clsx";
+import { createTwc } from "react-twc";
 import { twMerge } from "tailwind-merge";
 
 /* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call */
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
+
+export const twx = createTwc({ compose: cn });
