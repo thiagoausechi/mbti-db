c7c98855a97db95f719430b306b73bc23fcf1c9e
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-10-18 03:23:12 +0000

    refactor: 💄 enhance layout styling for Home component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,9 +4,11 @@ export default function Home() {
   const t = useTranslations("HomePage");
 
   return (
-    <main>
-      <h1>{t("title")}</h1>
-      <p>{t("subtitle")}</p>
+    <main className="container mx-auto p-4 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="font-black text-md sm:text-4xl text-center">
+        {t("title")}
+      </h1>
+      <p className="text-center">{t("subtitle")}</p>
     </main>
   );
 }
