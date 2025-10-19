30f06a3509a15213c26d05a2a0b7d63148dca573
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-10-19 15:11:49 -0300

    refactor: 🏷️ update Messages type definitions

---
+++ messages/en.ts
@@ -0,0 +1,15 @@
+import { Messages } from "next-intl";
+
+const en: Messages = {
+  Metadata: {
+    title: "mbti-db",
+    description:
+      "A database for MBTI types, cognitive functions, and related information.",
+  },
+  HomePage: {
+    title: "Welcome to mbti-db!",
+    subtitle: "This is still a work in progress.",
+  },
+};
+
+export default en;
