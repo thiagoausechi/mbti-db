e03aa33e523d937cbc075088c1e6953b549a8964
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-10-20 16:27:16 -0300

    feat: ✨ add visibility enumeration

---
+++ src/shared/enums/profile/visibilities.ts
@@ -0,0 +1,14 @@
+import { Lock, LockOpen, ShieldHalf } from "lucide-react";
+import type { ComponentType } from "react";
+import { z } from "zod/mini";
+
+export const visibility = ["show", "hide", "protected"] as const;
+
+export const visibilitySchema = z.enum(visibility);
+export type Visibility = z.infer<typeof visibilitySchema>;
+
+export const visibilityIconMap: Record<Visibility, ComponentType> = {
+  show: LockOpen,
+  hide: Lock,
+  protected: ShieldHalf,
+} as const;
