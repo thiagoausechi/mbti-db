ccfc029f4bef73a4eb8e37f6fc9623ff33b9417c
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-10-20 09:39:16 -0300

    feat: ✨ implement `Card` component

---
+++ src/client/components/ui/card.tsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+import { merge } from "~/client/lib/merge-class-name";
+
+function Card({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card"
+      className={merge(
+        "bg-light-tone text-dark-tone shadow-elevation-neumorphic rounded-surface flex flex-col gap-6 py-surface ",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-header"
+      className={merge(
+        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-surface has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-title"
+      className={merge(
+        "leading-none font-black text-shadow-neumorphic text-3xl",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-description"
+      className={merge(
+        "text-light-tone-muted font-semibold text-sm",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardAction({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-action"
+      className={merge(
+        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardContent({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-content"
+      className={merge("px-surface", className)}
+      {...props}
+    />
+  );
+}
+
+function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-footer"
+      className={merge(
+        "flex items-center px-surface [.border-t]:pt-6",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+};
