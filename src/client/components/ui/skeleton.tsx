4e7623e136830de59af13d60963931d6dddb9354
Thiago Ausechi <thiago_ausechi@icloud.com>
2026-02-07 17:38:18 -0300

    refactor: ♻️ separate `Skeleton` and `Placeholder` components

--- src/client/components/ui/skeleton.tsx
+++ src/client/components/ui/skeleton.tsx
@@ -1,13 +1,25 @@
 import { merge } from "~/client/lib/merge-class-name";
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+interface SkeletonProps<T> {
+  data: T | null | undefined;
+  placeholder: React.ReactNode;
+  children: (data: T) => React.ReactNode;
+}
+
+function Skeleton<T>({ data, placeholder, children }: SkeletonProps<T>) {
+  if (data === null || data === undefined) return placeholder;
+
+  return children(data);
+}
+
+function Placeholder({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={merge("bg-border rounded-md animate-pulse", className)}
+      className={merge("bg-border rounded-full animate-pulse", className)}
       {...props}
     />
   );
 }
 
-export { Skeleton };
+export { Placeholder, Skeleton };
