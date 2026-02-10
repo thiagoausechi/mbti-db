5a21e606386205817b29bc0923aca590c07db2a2
Thiago Ausechi <thiago_ausechi@icloud.com>
2026-02-10 13:28:47 -0300

    fix: 🔥 remove className from SidebarMenuSubButton

--- src/client/components/ui/sidebar.tsx
+++ src/client/components/ui/sidebar.tsx
@@ -453,15 +453,7 @@ function SidebarMenuSubButton({
   }) {
   return useRender({
     defaultTagName: "a",
-    props: mergeProps<"a">(
-      {
-        className: merge(
-          "text-dark-tone ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebar-accent active:text-sidebar-accent-foreground [&>svg]:text-sidebar-accent-foreground data-active:bg-sidebar-accent data-active:text-sidebar-accent-foreground h-7 gap-2 rounded-md px-2 focus-visible:ring-2 data-[size=md]:text-sm data-[size=sm]:text-xs [&>svg]:size-4 flex min-w-0 -translate-x-px items-center overflow-hidden outline-hidden group-data-[collapsible=icon]:hidden disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:shrink-0",
-          className,
-        ),
-      },
-      props,
-    ),
+    props: mergeProps<"a">({ className }, props),
     render,
     state: {
       slot: "sidebar-menu-sub-button",
