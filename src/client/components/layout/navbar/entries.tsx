faf84e116d1c8899c2e67123061e85456270a578
Thiago Ausechi <thiago_ausechi@icloud.com>
2026-02-10 16:13:55 -0300

    feat: ✨ implement `Navbar Entries` components

---
+++ src/client/components/layout/navbar/entries.tsx
@@ -0,0 +1,105 @@
+import { ChevronRight, LucideIcon } from "lucide-react";
+import { PropsWithChildren } from "react";
+import { Button } from "~/client/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "~/client/components/ui/collapsible";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuItemLabel,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "~/client/components/ui/sidebar";
+import { Link, usePathname } from "~/i18n/navigation";
+
+export function LinkEntry({
+  label,
+  icon: Icon,
+  href,
+  asSubitem = false,
+}: {
+  label: string;
+  icon: LucideIcon;
+  href: string;
+  asSubitem?: boolean;
+}) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  const MenuItemComp = asSubitem ? SidebarMenuSubItem : SidebarMenuItem;
+  const MenuButtonComp = asSubitem ? SidebarMenuSubButton : SidebarMenuButton;
+
+  return (
+    <MenuItemComp>
+      <MenuButtonComp
+        isActive={isActive}
+        className="w-full"
+        tooltip={label}
+        render={
+          <Link href={href}>
+            <Button
+              variant={isActive ? "active" : "secondary"}
+              className="justify-start"
+              full
+            >
+              <Icon />
+              <SidebarMenuItemLabel>{label}</SidebarMenuItemLabel>
+            </Button>
+          </Link>
+        }
+      />
+    </MenuItemComp>
+  );
+}
+
+export function CategoryEntry({
+  label,
+  children,
+}: PropsWithChildren<{ label: string }>) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>{label}</SidebarGroupLabel>
+      <SidebarMenu>{children}</SidebarMenu>
+    </SidebarGroup>
+  );
+}
+
+export function CollapsibleEntry({
+  label,
+  icon: Icon,
+  children,
+}: PropsWithChildren<{
+  label: string;
+  icon: LucideIcon;
+}>) {
+  return (
+    <Collapsible className="group/collapsible">
+      <SidebarMenuItem>
+        <CollapsibleTrigger
+          render={
+            <SidebarMenuButton
+              tooltip={label}
+              render={
+                <Button variant="secondary" className="justify-start" full>
+                  <Icon />
+                  <SidebarMenuItemLabel>{label}</SidebarMenuItemLabel>
+                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[open]/collapsible:rotate-90 group-data-[collapsible=icon]:hidden" />
+                </Button>
+              }
+            />
+          }
+        />
+
+        <CollapsibleContent>
+          <SidebarMenuSub>{children}</SidebarMenuSub>
+        </CollapsibleContent>
+      </SidebarMenuItem>
+    </Collapsible>
+  );
+}
