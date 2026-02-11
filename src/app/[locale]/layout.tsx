c70d463b656f77cdd3378cd9e4810b3ffb75234a
Thiago Ausechi <thiago_ausechi@icloud.com>
2026-02-10 22:12:43 -0300

    feat: ✨ implement `ThemeProvider` component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
   setRequestLocale(locale);
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <body className={`${openSans.variable}`}>
         <Providers>
           <Navbar />
--- src/client/components/providers/index.tsx
+++ src/client/components/providers/index.tsx
@@ -2,13 +2,16 @@ import { NextIntlClientProvider } from "next-intl";
 import type { PropsWithChildren } from "react";
 import { SidebarProvider } from "~/client/components/ui/sidebar";
 import { TooltipProvider } from "~/client/components/ui/tooltip";
+import { ThemeProvider } from "./theme-provider";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <NextIntlClientProvider>
-      <TooltipProvider>
-        <SidebarProvider>{children}</SidebarProvider>
-      </TooltipProvider>
+      <ThemeProvider>
+        <TooltipProvider>
+          <SidebarProvider>{children}</SidebarProvider>
+        </TooltipProvider>
+      </ThemeProvider>
     </NextIntlClientProvider>
   );
 }
