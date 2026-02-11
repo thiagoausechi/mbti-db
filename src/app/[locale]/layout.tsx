ba5c9ff33e09b9f4b54ff25eba14ae779b379944
Thiago Ausechi <thiago_ausechi@icloud.com>
2026-02-10 22:10:35 -0300

    fix: 🐛 add lang attribute to html element for better accessibility

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
   setRequestLocale(locale);
 
   return (
-    <html>
+    <html lang={locale}>
       <body className={`${openSans.variable}`}>
         <Providers>
           <Navbar />
